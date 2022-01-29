d3487fa001f7cebdd7d37e66e955cb194c371aa3
jemi-2 <91508803+jemi-2@users.noreply.github.com>
2022-01-28 18:56:32 -0800

    Add files via upload

---
+++ form.js
@@ -0,0 +1,7 @@
+class Form{
+    constructor(){
+        this.input=createInput("").attribute("placeholder","enter your name")
+        this.playButton = createButton("PLAY")
+        this.titleImg = createImg("")
+    }
+}
